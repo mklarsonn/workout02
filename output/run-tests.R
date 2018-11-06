2a87f6aa31d48de844ba17907f54a509c6382268
mklarsonn <mlarson@skycatch.com>
2018-11-05 19:51:49 -0800

    minkowski tests added

---
+++ output/run-tests.R
@@ -0,0 +1,8 @@
+library('testthat')
+
+functions <- dir('../code/functions')
+lapply(pasteO('../code/functions/', functions), source)
+
+sink(file = 'test-output.txt')
+test_dir('../code/tests')
+sink()
