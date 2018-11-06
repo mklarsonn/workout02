1259c99532522bbd3f556b4a42440ebb7bb95869
mklarsonn <mlarson@skycatch.com>
2018-11-05 22:29:40 -0800

    finished reverse string sdf exit()

---
+++ code/functions/reverse-chars.R
@@ -0,0 +1,12 @@
+#Minkowski Distance
+#' @title reverse-chars
+#' @description reverses the characters in a string in R
+#' @param x a string
+#' @return the reverse of a string
+
+reverse_chars <- function(x){
+  characters <- nchar(x)
+  reverse <- paste(substring(x, characters:1, characters:1), collapse = "")
+  return(reverse)
+  }
+reverse_chars("Hello my name is MK Larson")
